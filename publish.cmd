c172f433071da8f42c5f07600acfba92c7649520
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-15 20:38:19 -0700

    Initial commit.  Elder Bot repeats back what you say to it.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=elderbot-Web-Deploy.pubxml -p:Password=nnqDP7mfTlxdaqyA9nFEokL21yDxkxtJJ0vsjDjebNBZjJzswqK7azzcnm2F
+
