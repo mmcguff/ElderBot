8dea573d0b47cc3ceed0b6f6bbe37c6d192c94c3
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-17 06:13:56 -0700

    I have added a menu.  It works in web chat but renders poorly and does werid stuff in slack.  I will need to make some adjustments.

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Net.Http;
-
+using System.Collections.Generic;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -12,11 +12,44 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
     public class EchoDialog : IDialog<object>
     {
         protected int count = 1;
+        public enum Topics
+        {
+            AreMormonsChristian,
+            god,
+            trinity,
+        }
 
         public async Task StartAsync(IDialogContext context)
         {
-            context.Wait(MessageReceivedAsync);
+            context.Wait(GetTopic);
         }
+
+        public virtual async Task GetTopic(IDialogContext context, IAwaitable<IMessageActivity> activity)
+        {
+            var message = await activity;
+            var descriptions = new string[] { "Are Mormons Christian?", "What do Mormons believe about God?", "What do Mormons believe about the Trinity?" };
+
+            PromptDialog.Choice(
+                context: context,
+                resume: ChoiceReceivedAsync,
+                options: (IEnumerable<Topics>)Enum.GetValues(typeof(Topics)),
+                prompt: "Please select a topic about the LDS Church to learn more:",
+                retry: "Topic not available. Please try again.",
+                promptStyle: PromptStyle.Auto,
+                descriptions: descriptions
+                );
+        }
+
+        public virtual async Task ChoiceReceivedAsync(IDialogContext context, IAwaitable<Topics> activity)
+        {
+            Topics response = await activity;
+            await context.PostAsync($"Your made a chocice of {response.ToString()}");
+
+            //context.Call<object>(new AnnualPlanDialog(response.ToString()), ChildDialogComplete);
+
+        }
+
+
 
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
